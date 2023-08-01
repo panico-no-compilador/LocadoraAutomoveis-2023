483e3ef32ad1faf749089db0db6f1852e8e24294
caiovallee <caiohgvalle.14@gmail.com>
2023-08-01 15:55:45 -0300

    refatora int  id para guid

--- LocadoraAutomoveis.Dominio/Compartilhado/EntidadeBase.cs
+++ LocadoraAutomoveis.Dominio/Compartilhado/EntidadeBase.cs
@@ -2,7 +2,7 @@
 {
     public abstract class EntidadeBase<T>
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
 
         public abstract void Atualizar(T registro);
     }
