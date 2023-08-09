190fea7fcaa49f4424e43b0447d7c30fe56c9d2f
Davi Borges <davifelipe_300@hotmail.com>
2023-08-09 15:38:23 -0300

    resolve erro do merge

--- LocadoraAutomoveis.Infra.Orm/Migrations/20230809175806_MigrationMarge.Designer.cs
+++ LocadoraAutomoveis.Infra.Orm/Migrations/20230809175806_MigrationMarge.Designer.cs
@@ -12,8 +12,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LocadoraAutomoveis.Infra.Orm.Migrations
 {
     [DbContext(typeof(LocadoraAutomoveisDbContext))]
-    [Migration("20230803180527_Parceiro-Cupom")]
-    partial class ParceiroCupom
+    [Migration("20230809175806_MigrationMarge")]
+    partial class MigrationMarge
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
@@ -64,6 +64,67 @@ namespace LocadoraAutomoveis.Infra.Orm.Migrations
                     b.ToTable("TBParceiros", (string)null);
                 });
 
+            modelBuilder.Entity("LocadoraAutomoveis.Dominio.ModuloGrupoAutomoveis.GrupoAutomovel", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .HasColumnType("uniqueidentifier");
+
+                    b.Property<string>("Tipo")
+                        .IsRequired()
+                        .HasColumnType("varchar(100)");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("TBGrupoAutomoveis", (string)null);
+                });
+
+            modelBuilder.Entity("LocadoraAutomoveis.Dominio.ModuloPlanosCobranca.PlanoCobranca", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .HasColumnType("uniqueidentifier");
+
+                    b.Property<Guid>("CategoriaGrupAutoId")
+                        .HasColumnType("uniqueidentifier");
+
+                    b.Property<int>("KmDisponiveis")
+                        .HasColumnType("int");
+
+                    b.Property<decimal>("PrecoDiaria")
+                        .HasColumnType("decimal(18,2)");
+
+                    b.Property<decimal>("PrecoKm")
+                        .HasColumnType("decimal(18,2)");
+
+                    b.Property<int>("TipoPlano")
+                        .HasColumnType("int");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("CategoriaGrupAutoId");
+
+                    b.ToTable("TBPlanoCobranca", (string)null);
+                });
+
+            modelBuilder.Entity("LocadoraAutomoveis.Dominio.ModuloTaxasServicos.TaxasServicos", b =>
+                {
+                    b.Property<Guid>("Id")
+                        .HasColumnType("uniqueidentifier");
+
+                    b.Property<string>("Nome")
+                        .IsRequired()
+                        .HasColumnType("varchar(100)");
+
+                    b.Property<int>("PlanoCalculo")
+                        .HasColumnType("int");
+
+                    b.Property<decimal>("Preco")
+                        .HasColumnType("decimal(18,2)");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("TBTaxasServicos", (string)null);
+                });
+
             modelBuilder.Entity("LocadoraAutomoveis.Dominio.ModuloCuponsParceiros.Cupom", b =>
                 {
                     b.HasOne("LocadoraAutomoveis.Dominio.ModuloCuponsParceiros.Parceiro", "Parceiro")
@@ -74,6 +135,18 @@ namespace LocadoraAutomoveis.Infra.Orm.Migrations
                         .HasConstraintName("FK_TBCupom_TBParceiro");
 
                     b.Navigation("Parceiro");
+                });
+
+            modelBuilder.Entity("LocadoraAutomoveis.Dominio.ModuloPlanosCobranca.PlanoCobranca", b =>
+                {
+                    b.HasOne("LocadoraAutomoveis.Dominio.ModuloGrupoAutomoveis.GrupoAutomovel", "CategoriaGrupAuto")
+                        .WithMany()
+                        .HasForeignKey("CategoriaGrupAutoId")
+                        .OnDelete(DeleteBehavior.NoAction)
+                        .IsRequired()
+                        .HasConstraintName("FK_PlanoCobranca_GrupoAutomoveis");
+
+                    b.Navigation("CategoriaGrupAuto");
                 });
 
             modelBuilder.Entity("LocadoraAutomoveis.Dominio.ModuloCuponsParceiros.Parceiro", b =>
